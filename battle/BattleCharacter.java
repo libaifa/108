381d7a9b730761fe3481f4ac325505e46263cd19
libaifa <libaifa@users.noreply.github.com>
2014-04-07 09:58:42 -0600

    Update BattleCharacter.java

--- battle/BattleCharacter.java
+++ battle/BattleCharacter.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+
 public class BattleCharacter {
+  
+  private final String originalName;
+  private String charName;
+  private int charNumber = 1;
+  private boolean isPlayer;
+  
+  private int maxHealth, cHealth, maxEnergy, cEnergy;
+  private int str, mag, ten, ref;
+  
+  private ArrayList<ElementEffectiveness> eEffects;
+  
+  private ArrayList<Move> knownMoves;
+  private ArrayList<ActiveSkill> knownSkills;
+  
+  private TurnPreference turnPreference;
+  private BattleAI ai;
+  private BattleState battleState;
 
 }
